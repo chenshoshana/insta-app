c105325a1ba349d321cab35f1c68e7208b062b8b
chenshoshana <chenshoshana22@gmail.com>
2021-01-17 20:04:27 +0200

    redyx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { AppHeader } from './cmps/AppHeader.jsx'
+// import { AppHeader } from './cmps/AppHeader.jsx'
 import { Home } from './pages/Home.jsx'
 // import { About } from './pages/About.jsx'
 // import Login from './pages/Login.jsx'
@@ -8,22 +8,22 @@ import { Home } from './pages/Home.jsx'
 // import { UserDetails } from './pages/UserDetails.jsx'
 // import { HostDetails } from './pages/HostDetails.jsx'
 // import { ChatApp } from './cmps/ChatApp.jsx'
-import { InstaApp } from './pages/InstaApp.jsx'
-import { PostsList } from './pages/PostsList.jsx'
-import { PostDetails } from './cmps/PostDetails.jsx'
-import { PostEdit } from './pages/PostEdit.jsx'
-import { AppFooter } from './cmps/AppFooter.jsx'
-import './assets/scss/main.scss';
+// import { InstaApp } from './pages/InstaApp.jsx'
+// import { PostsList } from './pages/PostsList.jsx'
+// import { PostDetails } from './cmps/PostDetails.jsx'
+// import { PostEdit } from './pages/PostEdit.jsx'
+// import { AppFooter } from './cmps/AppFooter.jsx'
+// import './assets/scss/main.scss';
 
 
 
 export class App extends Component {
     render() {
         return (<React.Fragment>
-            <AppHeader />
+            {/* <AppHeader /> */}
             <Switch>
                 {/* <Route component={PostEdit} path="/edit/:postId?" /> */}
-                <Route component={PostDetails} path="/post/:postId" />
+                {/* <Route component={PostDetails} path="/post/:postId" /> */}
                 {/* <Route component={ChatApp} path="/chat" /> */}
                 {/* <Route component={Admin} path="/admin/userId:" /> */}
                 {/* <Route component={UserDetails} path="/user/userId:" /> */}
@@ -32,7 +32,7 @@ export class App extends Component {
                 {/* <Route component={Login} path="/login" /> */}
                 <Route component={Home} path="/" />
             </Switch>
-            <AppFooter />
+            {/* <AppFooter /> */}
         </React.Fragment>
         )
     }

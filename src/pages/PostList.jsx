e1806d818bbaa5499a62e587527cc4d8e2d23e5c
chenshoshana <chenshoshana22@gmail.com>
2021-01-21 11:11:20 +0200

    fff

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -5,7 +5,7 @@ export function PostList({ posts,onRemove }) {
     return (
         <div className="post-list">
             {posts.map(post => {
-                return <PostPreview key={post._id} post={post} onRemove={onRemove} />
+                return <PostPreview className="post-preview" key={post._id} post={post} onRemove={onRemove} />
             })}
         </div>
     )
